049e3e583d7e291bec8ba35535e5358da120b6ba
Sehrish Balouch <sehrishbalouch4@gmail.com>
2020-01-06 19:42:28 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,6 +1,7 @@
 class Triangle
   attr_accessor :side_1, :side_2, :side_3
-  def initialize()
+  def initialize(side_1, side_2, side_3)
+    
  
   class TriangleError < StandardError
     # triangle error code
